efe28d9860b5a7e51f3b5f7226a8b39e0847c429
jui1ee <150470502+jui1ee@users.noreply.github.com>
2026-04-27 21:54:34 +0530

    Added code for downloading nltk libraries

---
+++ download_nltk.py
@@ -0,0 +1,10 @@
+import nltk
+print("Downloading NLTK data...")
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+print("Done!") 
